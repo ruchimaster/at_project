43b819ff15460aac33aafe0422b4ee4eaff688bf
ruchimaster <master2006ruchi@gmail.com>
2026-08-11 21:27:27 +0530

    app.js added

---
+++ backend/app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/api/test", (req, res) => {
+  res.json({
+    message: "Food Waste Reduction API is working",
+  });
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
